169f67d5247eabef14e144d51187aa3e8d2415fe
Netzwerk_Atze <Netzwerk_Atze@WIN-DEV>
2025-02-28 12:32:57 +0100

    first few lines.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Hello, please enter your name: ");
+
+string name = Console.ReadLine();
+int namelength = name.Length;
+char firstletter = name[0];
+
+Console.WriteLine("hello " + name + "!");
+Console.WriteLine("Your name has " + namelength + " letters.");
+Console.Write("The first letter is " + firstletter + ".");
