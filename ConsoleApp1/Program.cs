b34003767efd72e853a586080c024e2ebe0e1063
Netzwerk_Atze <Netzwerk_Atze@WIN-DEV>
2025-03-01 22:54:18 +0100

    Add name validation and basic error handling

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 
-Console.Write("Hello, please enter your name: ");
+class Programm
+{
+    static void Main()
+    {
+        Console.Write("Hello, please enter your name: ");
 
-string name = Console.ReadLine();
-int namelength = name.Length;
-char firstletter = name[0];
+        string name = Console.ReadLine();
+        bool valid = IsValidName(name);
 
-Console.WriteLine("hello " + name + "!");
-Console.WriteLine("Your name has " + namelength + " letters.");
-Console.Write("The first letter is " + firstletter + ".");
+        if (valid)
+        {
+            try
+            {
+                int namelength = name.Length;
+                char firstletter = name[0];
+
+                Console.WriteLine("Hello " + char.ToUpper(name[0]) + name.Substring(1) + "!");
+                Console.WriteLine("Your name has " + namelength + " letters.");
+                Console.Write("The first letter is " + firstletter + ".");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("That's not a name!");
+        }
+    }
+    static bool IsValidName(string name)
+    {
+        foreach (char letter in name)
+        {
+            if (!char.IsLetter(letter))
+            {
+                return false;
+            }
+        }                 
+        return true;
+    }
+}
